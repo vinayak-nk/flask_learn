c844a8baa98ed11e8cda484151d703b330ca6b55
Vinayak K <vinayakkurugovinakoppa@qnap.com>
2022-03-08 21:40:50 +0530

    initial set up

---
+++ flask_blog/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'vk',
+        'title': 'Blogpost 1',
+        'content': 'First post content',
+        'date_posted': 'March, 7 2022'
+    },
+    {
+        'author': 'vnk',
+        'title': 'Blogpost 2',
+        'content': 'Second post content',
+        'date_posted': 'March, 8 2022'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    # return "<p>Hello.---------!!!</p>"
+    return render_template('home.html', posts=posts, title="xyz")
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+    # return "<p>About.---------!!!</p>"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
