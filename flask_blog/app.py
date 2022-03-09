24208268fee4f6ca15d6ebd74b1e32731fdef992
Vinayak K <vinayakkurugovinakoppa@qnap.com>
2022-03-09 14:07:34 +0530

    forms, SECRET_KEY, form validation, pip list

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e4fbcd72d53972a48b59a0807882e41a' #import secrets, secrets.token_hex(16)
 
 posts = [
     {
@@ -30,6 +33,29 @@ def about():
     return render_template('about.html')
     # return "<p>About.---------!!!</p>"
 
+
+@app.route("/register",  methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        
+        return redirect(url_for('home'))
+    
+    return render_template('register.html', title='Registre', form=form)
+
+@app.route("/login",  methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'vnk@gmail.com' and form.password.data == '123':
+            flash('Logged In!!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Logged Failed', 'danger')
+            return redirect(url_for('home'))    
+    return render_template('login.html', title="Login", form=form)
 
 
 if __name__ == '__main__':
